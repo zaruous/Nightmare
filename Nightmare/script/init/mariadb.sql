0fa661ff4b964b71f4dba86ffd16f1397d08e2f3
zaruous <zaruous@naver.com>
2024-06-09 23:36:09 +0900

    .

--- Nightmare/script/init/mariadb.sql
+++ Nightmare/script/init/mariadb.sql
@@ -17,25 +17,50 @@
 CREATE DATABASE IF NOT EXISTS `nightmare` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
 USE `nightmare`;
 
+-- 테이블 nightmare.chat_history 구조 내보내기
+CREATE TABLE IF NOT EXISTS `chat_history` (
+  `ID` double NOT NULL,
+  `QUESTION` text DEFAULT NULL,
+  `ANSWER` text DEFAULT NULL,
+  `FIRST_REGER_ID` enum('USER','AI') DEFAULT NULL,
+  `FST_REG_DT` timestamp NULL DEFAULT sysdate(),
+  `AI_ID` double DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
 -- 테이블 nightmare.tbm_sm_cnf 구조 내보내기
 CREATE TABLE IF NOT EXISTS `tbm_sm_cnf` (
+  `ID` double NOT NULL DEFAULT sysdate(),
   `GROUP` varchar(50) NOT NULL,
   `KEY` varchar(50) NOT NULL,
   `VALUE` varchar(500) DEFAULT NULL,
   `FST_REG_DT` datetime DEFAULT sysdate(),
   `FNL_UPD_DT` datetime DEFAULT NULL,
-  `CNF_CMF_1` varchar(100) DEFAULT NULL,
-  `CNF_CMF_2` varchar(100) DEFAULT NULL,
-  `CNF_CMF_3` varchar(100) DEFAULT NULL,
-  `CNF_CMF_4` varchar(100) DEFAULT NULL,
-  `CNF_CMF_5` varchar(100) DEFAULT NULL,
-  `CNF_CMF_6` varchar(100) DEFAULT NULL,
-  `CNF_CMF_7` varchar(100) DEFAULT NULL,
+  `CNF_CMF_1` varchar(100) DEFAULT NULL COMMENT 'model',
+  `CNF_CMF_2` varchar(100) DEFAULT NULL COMMENT 'api key',
+  `CNF_CMF_3` varchar(100) DEFAULT NULL COMMENT 'language',
+  `CNF_CMF_4` varchar(100) DEFAULT NULL COMMENT 'prompt',
+  `CNF_CMF_5` varchar(100) DEFAULT NULL COMMENT 'response_format',
+  `CNF_CMF_6` varchar(100) DEFAULT NULL COMMENT 'voice',
+  `CNF_CMF_7` varchar(100) DEFAULT NULL COMMENT 'voice speed',
   `CNF_CMF_8` varchar(100) DEFAULT NULL,
   `CNF_CMF_9` varchar(100) DEFAULT NULL,
   `CNF_CMF_10` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`GROUP`,`KEY`)
+  PRIMARY KEY (`ID`,`GROUP`,`KEY`),
+  UNIQUE KEY `ID` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='시스템 설정 관련 테이블';
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 nightmare.tbm_sm_prompts 구조 내보내기
+CREATE TABLE IF NOT EXISTS `tbm_sm_prompts` (
+  `GROUP` varchar(50) NOT NULL,
+  `ID` varchar(50) NOT NULL,
+  `PROMPT` text DEFAULT NULL,
+  PRIMARY KEY (`GROUP`,`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
